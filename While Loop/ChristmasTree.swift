1b9f2f20054f77277ce53eaf0cbee951c8d0428e
supxx <supx@mail.com>
2016-05-18 23:20:01 +0800

    Add files via upload

---
+++ While Loop/ChristmasTree.swift
@@ -0,0 +1,53 @@
+func tree(){
+
+	let num = 15  //You can config this num to set tree-level
+
+	var i = 1
+
+	while i < num {
+
+		var null = ""
+	
+		var j = num
+
+		while j > i {
+
+			null += " " 
+
+			j-=1
+
+		}
+
+		var k = 0
+
+		var star = ""
+
+		while k < i {
+
+
+			if k == 0 {
+
+				star += "*"
+			}
+
+			else {
+
+				star += "**"
+
+			}
+
+			k+=1
+		
+		}
+
+		print(null,star)
+
+		i+=1
+	}
+
+}
+
+
+
+
+tree()
